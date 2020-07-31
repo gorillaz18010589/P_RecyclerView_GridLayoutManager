3322ba5f8e1a876d6db5af8bc9bbd07c4192e633
jennifer <gorillaz1801058@gmail.com>
2020-07-31 22:52:52 +0800

    v2 新增setSpanSizeLookup,第一筆資料占一列,其他一列四筆

--- app/src/main/java/com/example/p_recyclerview_gridlayoutmanager/MainActivity.java
+++ app/src/main/java/com/example/p_recyclerview_gridlayoutmanager/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Game> games;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,21 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         games = new ArrayList<>();
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
-        games.add(new Game("王者榮耀","10000"));
 
-        BlanceRecyclerViewAdapter blanceRecyclerViewAdapter = new BlanceRecyclerViewAdapter(games,this);
+        for (int i = 0; i <= 100; i++) {
+            games.add(new Game("王者榮耀", "10000"));
+        }
 
+
+        BlanceRecyclerViewAdapter blanceRecyclerViewAdapter = new BlanceRecyclerViewAdapter(games, this);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                // 显示的列数 = spanCount / spanSize ;
+                if (position % 17 == 0 && position != 17 && position != 34 && position != 51
+                        && position != 68 && position != 85) {
+                    //SpanSize 返回4 代表该行只显示1列    列数 = 4/4 =1
+                    Log.v("hank", "position4:" + position + "%:" + position % 1);
+                    return 4;
+                } else {
+                    //SpanSize 返回1代表该行显示4列    列数 = 4/1 =4
+                    Log.v("hank", "position:" + position);
+                    return 1;
+                }
+            }
+        });
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,4));
+        recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(blanceRecyclerViewAdapter);
 
     }
